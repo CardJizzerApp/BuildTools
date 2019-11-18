01541a23e66ef30c02dd9066e43b97c232cef00c
Tobias Staudacher <tobias.staudacher@outlook.de>
2019-11-18 12:18:26 +0100

    Init simple deploy

--- webHookDeploy/container/deploy.sh
+++ webHookDeploy/container/deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+current_directory="~/cardjizzer-deploy"
+
+mkdir -p $current_directory
+git clone https://github.com/CardJizzerApp/BuildTools.git $current_directory
+ls $current_directory
+chmod +x $current_directory/webHookDeploy/host/run.sh
+./$current_directory/webHookDeploy/host/run.sh
 docker-compose up -D
