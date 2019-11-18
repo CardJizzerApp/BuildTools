07a7a1b68ab3ab75282f892f5782294ce2c8b933
IJustDev <alexanderpanovi@yahoo.de>
2019-11-18 14:58:53 +0100

    Update run.sh

--- webHookDeploy/host/run.sh
+++ webHookDeploy/host/run.sh
@@ -2,5 +2,5 @@
 
 docker run -p 9000:9000 \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /home/cardjizzer-webhook/container:/etc/webhook \
-    cardjizzer:webhook
+    -v /home/cardjizzer-webhook/webHookDeploy/container:/etc/webhook \
+    cardjizzer:webhook -verbose -hooks=/etc/webhook/hooks.json
